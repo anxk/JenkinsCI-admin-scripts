71d3bc66521411b19f420309e47b9850ea2d90d5
anxk <xiaokang112358@gmail.com>
2019-11-25 21:47:06 +0800

    addNodeLabels.groovy

---
+++ scripts/addNodeLabels.groovy
@@ -0,0 +1,12 @@
+#!/bin/groovy
+// add labels to specific node set
+
+def targetNodeNames = ['test1', 'test2']
+def additionalLabels = 'testLabel'
+
+def h = Jenkins.get()
+h.nodes.each {
+	if (it.name in targetNodeNames) {
+		it.setLabelString(it.getLabelString() + ' ' + additionalLabels)
+	}
+}
